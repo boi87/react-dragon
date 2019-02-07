f616facf78f1116e973e078525950e046712287a
mike <boimik87@gmail.com>
2019-02-07 10:52:55 +0000

    passing apple coordinates

--- apple.js
+++ apple.js
@@ -1,34 +1,34 @@
-import React from "react";
-import { render } from "react-dom";
+import React from 'react';
+import { render } from 'react-dom';
 // import Cell from "./Cell";
 
 const CELL_SIZE = 20;
-const ax = Math.floor(Math.random() * 39);
-const ay = Math.floor(Math.random() * 29);
+// const ax = Math.floor(Math.random() * 39);
+// const ay = Math.floor(Math.random() * 29);
 
-class Apple extends React.Component {
-  state = {
-    x: ax,
-    y: ay
-    // log: console.log("diocan", this.state.x, this.state.y)
-  };
+const Apple = props => (
+    // state = {
+    //     x: Math.floor(Math.random() * 39),
+    //     y: Math.floor(Math.random() * 29)
+    //     // log: console.log("diocan", this.state.x, this.state.y)
+    // };
 
-  apple = () => {
-    console.log("apple in apple.js,", this.state.x, this.state.y);
-  };
-  render() {
-    return (
-      <div
+    // apple = () => {
+    //     console.log('apple in apple.js,', this.props.x, this.props.y);
+    // };
+    // render() {
+    // return (
+    <div
         className="Apple"
         style={{
-          left: `${CELL_SIZE * ax + 1}px`,
-          top: `${CELL_SIZE * ay + 1}px`,
-          width: `${CELL_SIZE - 1}px`,
-          height: `${CELL_SIZE - 1}px`
+            left: `${CELL_SIZE * props.x + 1}px`,
+            top: `${CELL_SIZE * props.y + 1}px`,
+            width: `${CELL_SIZE - 1}px`,
+            height: `${CELL_SIZE - 1}px`
         }}
-      />
-    );
-  }
-}
+    />
+);
+// }
+// );
 
 export default Apple;
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { render } from "react-dom";
-import "./dragon.css";
-import Cell from "./Cell";
-import Apple from "./apple";
+import React from 'react';
+import { render } from 'react-dom';
+import './dragon.css';
+import Cell from './Cell';
+import Apple from './apple';
 
 // import makeEmpyBoard from "./Makeboard.js";
 // import makecells from "./Makeboard";
@@ -12,206 +12,222 @@ const WIDTH = 800;
 const HEIGHT = 600;
 
 class Dragon extends React.Component {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    // dividing the height and width for the nr of cells
-    this.rows = HEIGHT / CELL_SIZE;
-    this.cols = WIDTH / CELL_SIZE;
-    this.board = this.makeEmptyBoard();
-  }
-  componentDidMount() {
-    window.addEventListener("keydown", this.handleKeyPress);
-  }
-
-  // initial snake position
-  state = {
-    cells: [
-      { x: 7, y: 5 },
-      { x: 6, y: 5 },
-      { x: 5, y: 5 },
-      { x: 4, y: 5 },
-      { x: 3, y: 5 },
-      { x: 2, y: 5 }
-    ],
-    running: false,
-    interval: 500,
-    direction: "right",
-    apple: [{ x: Apple.ax, y: Apple.ay }]
-  };
-
-  //   Create empty board
-  makeEmptyBoard() {
-    let board = [];
-    ``;
-    for (let y = 0; y < this.rows; y++) {
-      board[y] = [];
-      for (let x = 0; x < this.cols; x++) {
-        board[y][x] = false;
-      }
+        // dividing the height and width for the nr of cells
+        this.rows = HEIGHT / CELL_SIZE;
+        this.cols = WIDTH / CELL_SIZE;
+        this.board = this.makeEmptyBoard();
     }
-    return board;
-  }
-
-  //   Create cells from this.board
-  makeCells() {
-    console.log("in the makecells");
-
-    let cells = [];
-    for (let y = 0; y < this.rows; y++) {
-      for (let x = 0; x < this.cols; x++) {
-        if (this.board[y][x]) {
-          cells.push({ x, y });
-        }
-      }
-    }
-    return cells;
-  }
-
-  // switch directions and handle keypress
-  handleKeyPress = event => {
-    // console.log(event);
-    let direction;
-    const snakeHead = this.state.cells[0];
-    console.log(this.state.cells);
-    console.log("apple in index.js", this.state.Apple);
-
-    switch (event.keyCode) {
-      case 37:
-        event.preventDefault();
-        console.log("you hit left");
-        this.state.direction != "right"
-          ? (direction = "left")
-          : (direction = "right");
-        break;
-      case 38:
-        event.preventDefault();
-        console.log("you hit up");
-        this.state.direction != "down"
-          ? (direction = "up")
-          : (direction = "down");
-        break;
-      case 39:
-        event.preventDefault();
-        console.log("you hit right");
-        this.state.direction != "left"
-          ? (direction = "right")
-          : (direction = "left");
-        break;
-
-      case 40:
-        event.preventDefault();
-        console.log("you hit down");
-        this.state.direction != "up"
-          ? (direction = "down")
-          : (direction = "up");
-        break;
+    componentDidMount() {
+        window.addEventListener('keydown', this.handleKeyPress);
     }
 
-    // const nextCell = { x, y };
-
-    console.log("this is snakehead", snakeHead);
-    // console.log('nextcell!', nextCell);
-
-    // setInterval(() => {
-    this.setState({
-      direction: direction
-    });
-    // }, this.state.interval);
-  };
-
-  calculateNextCell = () => {
-    const snakeHead = this.state.cells[0];
-    let x, y;
-    switch (this.state.direction) {
-      case "right":
-        x = snakeHead.x + 1;
-        y = snakeHead.y;
-
-        break;
-      case "left":
-        x = snakeHead.x - 1;
-        y = snakeHead.y;
-
-        break;
-      case "down":
-        y = snakeHead.y + 1;
-        x = snakeHead.x;
-
-        break;
-      case "up":
-        y = snakeHead.y - 1;
-        x = snakeHead.x;
-
-        break;
-    }
-
-    const nextCell = { x, y };
-
-    this.setState({
-      cells: [nextCell, ...this.state.cells].slice(0, this.state.cells.length),
-      apple: [{ x: this.props.ax, y: this.props.ay }]
-    });
-
-    if (
-      snakeHead.x < 0 ||
-      snakeHead.x > 39 ||
-      snakeHead.y > 29 ||
-      snakeHead.y < 0
-    ) {
-      console.log("you died");
-      this.setState({
+    // initial snake position
+    state = {
         cells: [
-          { x: 7, y: 5 },
-          { x: 6, y: 5 },
-          { x: 5, y: 5 },
-          { x: 4, y: 5 },
-          { x: 3, y: 5 },
-          { x: 2, y: 5 }
-        ]
-        // direction: this.right
-      });
+            { x: 7, y: 5 },
+            { x: 6, y: 5 },
+            { x: 5, y: 5 },
+            { x: 4, y: 5 },
+            { x: 3, y: 5 },
+            { x: 2, y: 5 }
+        ],
+        running: false,
+        interval: 500,
+        direction: 'right',
+        apple: {
+            ax: Math.floor(Math.random() * 39),
+            ay: Math.floor(Math.random() * 29)
+        }
+
+        // apple: [{ x: Apple.x, y: Apple.y }]
+    };
+
+    //   Create empty board
+    makeEmptyBoard() {
+        let board = [];
+        ``;
+        for (let y = 0; y < this.rows; y++) {
+            board[y] = [];
+            for (let x = 0; x < this.cols; x++) {
+                board[y][x] = false;
+            }
+        }
+        return board;
     }
-  };
 
-  handleClick = event => {
-    clearInterval();
-    setInterval(() => {
-      this.calculateNextCell();
-    }, this.state.interval);
-  };
+    //   Create cells from this.board
+    makeCells() {
+        console.log('in the makecells');
 
-  // clear interval when unmount
+        let cells = [];
+        for (let y = 0; y < this.rows; y++) {
+            for (let x = 0; x < this.cols; x++) {
+                if (this.board[y][x]) {
+                    cells.push({ x, y });
+                }
+            }
+        }
+        return cells;
+    }
 
-  render() {
-    const { cells } = this.state;
-    return (
-      <div>
-        <h1>Release the dragon</h1>
-        <button id="btn_start" onClick={this.handleClick}>
-          start
-        </button>
-        {/* <button id="btn_stop" onClick=""> */}
-        {/* stop */}
-        {/* </button> */}
+    // switch directions and handle keypress
+    handleKeyPress = event => {
+        // console.log(event);
+        let direction;
+        const snakeHead = this.state.cells[0];
+        console.log(this.state.cells);
+        console.log(
+            'apple in index.js',
+            this.state.apple.ax,
+            this.state.apple.ay
+        );
 
-        <div
-          tabIndex="1"
-          className="Board"
-          style={{
-            width: WIDTH,
-            height: HEIGHT,
-            backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`
-          }}
-        >
-          {cells.map((cell, index) => (
-            <Cell x={cell.x} y={cell.y} key={`${cell.x},${cell.y}${index}`} />
-          ))}
-          <Apple />
-        </div>
-      </div>
-    );
-  }
+        switch (event.keyCode) {
+            case 37:
+                event.preventDefault();
+                console.log('you hit left');
+                this.state.direction != 'right'
+                    ? (direction = 'left')
+                    : (direction = 'right');
+                break;
+            case 38:
+                event.preventDefault();
+                console.log('you hit up');
+                this.state.direction != 'down'
+                    ? (direction = 'up')
+                    : (direction = 'down');
+                break;
+            case 39:
+                event.preventDefault();
+                console.log('you hit right');
+                this.state.direction != 'left'
+                    ? (direction = 'right')
+                    : (direction = 'left');
+                break;
+
+            case 40:
+                event.preventDefault();
+                console.log('you hit down');
+                this.state.direction != 'up'
+                    ? (direction = 'down')
+                    : (direction = 'up');
+                break;
+        }
+
+        // const nextCell = { x, y };
+
+        console.log('this is snakehead', snakeHead);
+        // console.log('nextcell!', nextCell);
+
+        // setInterval(() => {
+        this.setState({
+            direction: direction
+        });
+        // }, this.state.interval);
+    };
+
+    calculateNextCell = () => {
+        const snakeHead = this.state.cells[0];
+        let x, y;
+        switch (this.state.direction) {
+            case 'right':
+                x = snakeHead.x + 1;
+                y = snakeHead.y;
+
+                break;
+            case 'left':
+                x = snakeHead.x - 1;
+                y = snakeHead.y;
+
+                break;
+            case 'down':
+                y = snakeHead.y + 1;
+                x = snakeHead.x;
+
+                break;
+            case 'up':
+                y = snakeHead.y - 1;
+                x = snakeHead.x;
+
+                break;
+        }
+
+        const nextCell = { x, y };
+
+        this.setState({
+            cells: [nextCell, ...this.state.cells].slice(
+                0,
+                this.state.cells.length
+            ),
+            apple: [{ x: this.props.ax, y: this.props.ay }]
+        });
+
+        if (
+            snakeHead.x < 0 ||
+            snakeHead.x > 39 ||
+            snakeHead.y > 29 ||
+            snakeHead.y < 0
+        ) {
+            console.log('you died');
+            this.setState({
+                cells: [
+                    { x: 7, y: 5 },
+                    { x: 6, y: 5 },
+                    { x: 5, y: 5 },
+                    { x: 4, y: 5 },
+                    { x: 3, y: 5 },
+                    { x: 2, y: 5 }
+                ]
+                // direction: this.right
+            });
+        }
+    };
+
+    handleClick = event => {
+        clearInterval();
+        setInterval(() => {
+            this.calculateNextCell();
+        }, this.state.interval);
+    };
+
+    // clear interval when unmount
+
+    render() {
+        const { cells } = this.state;
+        return (
+            <div>
+                <h1>Release the dragon</h1>
+                <button id="btn_start" onClick={this.handleClick}>
+                    start
+                </button>
+                {/* <button id="btn_stop" onClick=""> */}
+                {/* stop */}
+                {/* </button> */}
+
+                <div
+                    tabIndex="1"
+                    className="Board"
+                    style={{
+                        width: WIDTH,
+                        height: HEIGHT,
+                        backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`
+                    }}
+                >
+                    {cells.map((cell, index) => (
+                        <Cell
+                            x={cell.x}
+                            y={cell.y}
+                            key={`${cell.x},${cell.y}${index}`}
+                        />
+                    ))}
+                    <Apple x={this.state.apple.ax} y={this.state.apple.ay} />
+                </div>
+            </div>
+        );
+    }
 }
 
 // render(<Dragon />, document.getElementById('root'));
